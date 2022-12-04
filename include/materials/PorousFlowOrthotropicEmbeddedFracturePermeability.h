c8621082bbeb10743dee4f445b6a642e3abbf1fc
iyevugah <iyevugah@icloud.com>
2022-12-03 20:54:31 -0600

    delete

--- include/materials/PorousFlowOrthotropicEmbeddedFracturePermeability.h
+++ include/materials/PorousFlowOrthotropicEmbeddedFracturePermeability.h
@@ -1,27 +1,27 @@
 #pragma once
 
-#include "PorousFlowPermeabilityBase.h"
+#include "PorousFlowEmbeddedFracturePermeabilityBase.h"
 #include "RankTwoTensor.h"
 #include <Eigen/Geometry>
 #include "libmesh/utility.h"
 
 /**
  * Material designed to provide the permeability tensor which is a function of
- * the computed strain. This permeability material is based on Embedded Fractures.
+ * the computed strain. This permeability material is based on Embedded Orthotropic Fractures.
  * See Zill et. al.(2021): Hydro-mechanical continuum modelling of fluid percolation
  * through rock. The permeability is given as follows:
  *
- * k = (k_m * I_{ij}) + (b/a*[(b^2/12 - k_m)]*(I-M_{ij}))
+ * k = k_m*I +  [b_i/a_i * (\frac{b_{i}^2}{12} - k_m)*(I-M_i)].
  *
- * where b is the fracture aperture given by: b_0 + /Delta{b}
- * /Delta{b} depends on the strain (/epsilon) as follows:
- * /Delta{b} = a * 〈/epsilon_n -/epsilon_0〉. Here, /epsilon_0 is a threshold strain
- * and /epsilon_n is the computed strain normal to the embedded fracture plane/surface.
- * a is mean fracture distance, b is fracture aperture, K_m is matrix/intrinsic permeability.
- * I_{ij} is the identity tensor and M_{ij} is a structure tensor given as n⊗n. n is a vector normal
- * to the fracture.
+ * where i=summation Over number of fracture planes/surfaces. b is the fracture
+ *  aperture given by: b_{i0} + /Delta{b_i} /Delta{b_i} depends on the strain (/epsilon) as follows:
+ * /Delta{b_i} = a_i * 〈/epsilon :M_i - /epsilon_{0i}〉. Here, /epsilon_{0i} is a threshold strain of
+ * the material in each of the fracture normal vector direction. /epsilon is total strain
+ * a_i and b_i are fracture distance and fracture aperture respectively in each fracture
+ * normal vector direction. K_m is the matrix/intrinsic permeability. I_{ij} is the identity tensor
+ * and M_{ij} is a structure tensor given as n_i⊗n_i. n_i is a vector normal to each fracture plane.
  */
-class PorousFlowOrthotropicEmbeddedFracturePermeability : public PorousFlowPermeabilityBase
+class PorousFlowOrthotropicEmbeddedFracturePermeability : public PorousFlowEmbeddedFracturePermeabilityBase
 {
 public:
 	static InputParameters validParams();
@@ -36,13 +36,13 @@ protected:
 	void computeQpProperties() override;
 
 	/// optional parameter that allows multiple mechanics materials to be defined
-  const std::string _base_name;
+    const std::string _base_name;
 
 	/// mean fracture distance in all 3 directions
-    std::vector<double> _a;
+    std::vector<double> _alpha;
 
 	/// Threshold strain in all 3 directions
-     std::vector<double> _e0;
+     std::vector<double> _eps;
 
 	/// matrix/intrinsic permeability
 	const Real _km;
@@ -51,7 +51,7 @@ protected:
 	bool _n_const;
 
 	/// Tensor that holds the normal vector to the fracture surface in each direction
-	RealTensorValue _nVec;
+	RealTensorValue _NVec;
 
 	/// string to hold the name of the fracture rotation angle around xy
 	std::string _phi_xy;
@@ -74,4 +74,7 @@ protected:
 // fracture rotation angles about xy and yz (in radians)
   	 const Real _rad_xy;
   	 const Real _rad_yz;
+
+// Jacobian factor
+	 const Real _jf;
 };
