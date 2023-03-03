9ad5b1a5dcd61ebefd14bb2a8272dd8457c066e9
iyevugah <iyevugah@icloud.com>
2023-03-03 00:30:14 -0600

    Orthotropic Embedded Fracture Permeability (all issues resolved)

--- src/materials/PFOrthoEM.C
+++ src/materials/PFOrthoEM.C
@@ -11,7 +11,7 @@
 /*    it under the terms of the GNU General Public License as published by    */
 /*      the Free Software Foundation, either version 3 of the License, or     */
 /*                     (at your option) any later version.                    */
-/*           Time was heavily invested in this codes. Please, show            */
+/*           Time was heavily invested in these codes. Please, show           */
 /*                 support by properly referencing it. Thanks.                */
 /*                                                                            */
 /*       This program is distributed in the hope that it will be useful,      */
